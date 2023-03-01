350e8fac29ee1850e5e743ca7307cc3192e34cd5
IShallRiseAgain <IShallRiseAgain239239@gmail.com>
2023-02-28 23:43:00 -0500

    added gender detection based on name

--- VoiceUtil.py
+++ VoiceUtil.py
@@ -2,11 +2,13 @@ import json
 import requests
 import random
 import pyttsx3
+import gender_guesser.detector as gender
 
 with open('ElevenLabs.txt', 'r') as infile:
     apiKey = infile.read()
-enable_elevenAI=True
-availableVoices ={}
+enable_elevenAI=False
+availableMaleVoices ={}
+availableFemaleVoices = {}
 narratorVoice="Josh"
 narratorName="narrator"
 voices = {}
@@ -18,28 +20,50 @@ def getVoiceList():
     for item in response["voices"]:
             if item["name"]==narratorVoice:
                 voices[narratorName]=item["voice_id"]
-            else:    
-                availableVoices[item["name"]]=item["voice_id"]
-                
+            else:
+                d = gender.Detector(case_sensitive=False)
+                if detect_gender(item["name"],d):
+                    availableFemaleVoices[item["name"].lower()]=item["voice_id"]
+                else:    
+                    availableMaleVoices[item["name"].lower()]=item["voice_id"]
+
+def detect_gender(text, detector):
+    genderCheck = detector.get_gender(text.lower().split(" ")[0])
+    if "female" in genderCheck:
+        return True
+    return False
+    
 def create_dialogue(line, text):
     curVoice="None"
     if(len(line)>1):
         dialogue=line[1]
-        if line[0] in voices:
-            curvoice=voices[line[0]]
+        if line[0].lower() in voices:
+            curvoice=voices[line[0].lower()]
         else:
-            key = random.choice(list(availableVoices.keys())) 
-            curvoice=voices[line[0]]= availableVoices[key]
-            availableVoices.pop(key)
+            d = gender.Detector(case_sensitive=False)
+            if detect_gender(line[0],d):
+                if len(availableFemaleVoices.keys())>0:
+                    key = random.choice(list(availableFemaleVoices.keys()))
+                    curvoice=voices[line[0].lower()]= availableFemaleVoices[key]
+                    availableFemaleVoices.pop(key)
+                else:
+                    curvoice=voices[line[0].lower()]=random.choice(list(voices.values()))
+            else:
+                if len(availableMaleVoices.keys())>0:
+                    key = random.choice(list(availableMaleVoices.keys()))
+                    curvoice=voices[line[0].lower()]= availableMaleVoices[key]
+                    availableMaleVoices.pop(key)
+                else:
+                    curvoice=voices[line[0].lower()]=random.choice(list(voices.values()))
         return [dialogue, curvoice]   
         
     else:
         if narratorName in voices:
             curvoice=voices[narratorName]
         else:
-            key = random.choice(list(availableVoices.keys())) 
-            curvoice=voices[narratorName]=availableVoices[key]
-            availableVoices.pop(key)
+            key = random.choice(list(availableMaleVoices.keys())) 
+            curvoice=voices[narratorName]=availableMaleVoices[key]
+            availableMaleVoices.pop(key)
         return [text, curvoice]
         
 def generate_voice_pyttsx3(path, ind, dialogue):
